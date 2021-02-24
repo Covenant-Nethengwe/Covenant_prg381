5091c080a87ba22e48a3d75a12112f8614a82736
Covenant Nethengwe <covenantn9@gmail.com>
2021-02-24 11:10:11 +0200

    Removing comments

--- prg381.java
+++ prg381.java
@@ -2,7 +2,6 @@ public class prg381{
     public static void main(String[] args) {
         System.out.println("==New java file on my repo==");
         System.out.println("You Love it here?");
-        //Comment
         System.out.println("Bet you love it here");
     }   
 }
