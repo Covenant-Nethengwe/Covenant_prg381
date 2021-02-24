86a12462543f4be3c1f0b02ff86cf6c290a1352b
Covenant Nethengwe <covenantn9@gmail.com>
2021-02-24 10:26:21 +0200

    First Comment

--- prg381.java
+++ prg381.java
@@ -2,6 +2,7 @@ public class prg381{
     public static void main(String[] args) {
         System.out.println("==New java file on my repo==");
         System.out.println("You Love it here?");
+        //Comment
     }
     
 }
